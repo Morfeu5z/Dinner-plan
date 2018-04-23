d5c3a8f60d922d6147b3612a205742ca52416d0e
Aleks <johnhenk1122@gmail.com>
2018-04-23 16:29:51 +0200

    Update do wersji stabilnej

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    render_template('index.html')
 
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="127.0.0.1", port=5000)
